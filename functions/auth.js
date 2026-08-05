cdef935aa1efbd80dac0bff5c0291cad6fca96dc
Jesseraaij <jesse.r@ziggo.nl>
2026-08-06 00:18:37 +0200

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,9 +1,9 @@
 export async function onRequestGet(context) {
+  const clientId = "Ov23li31OlxsOnqTuE08";
   const url = new URL(context.request.url);
-  const clientId = "Ov23li31OlxsOnqTuE08"; // Jouw GitHub App ID
+  const redirectUri = `https://${url.host}/callback`;
   
-  const redirectUri = `${url.origin}/callback`;
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo,user`;
-
+  
   return Response.redirect(githubAuthUrl, 302);
 }
