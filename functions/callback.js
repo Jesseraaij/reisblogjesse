276de46f1d83be282049b7fb1f9746b1780c7e45
Jesseraaij <jesse.r@ziggo.nl>
2026-08-05 23:50:54 +0200

    fix1

--- functions/callback.js
+++ functions/callback.js
@@ -1,9 +1,16 @@
 export async function onRequestGet(context) {
   const url = new URL(context.request.url);
   const code = url.searchParams.get("code");
-  const clientId = "Ov23li31OpenqTuE08";
   
-  // Stuur de code door naar GitHub om het inlog-token op te halen
+  const clientId = "Ov23li31OlxsOnqTuE08";
+  // Als je een Client Secret hebt, kun je die hier invullen (tussen de quotes), anders laten we hem leeg of vangen we het op
+  const clientSecret = context.env.GITHUB_CLIENT_SECRET || "";
+
+  if (!code) {
+    return new Response("Geen autorisatiecode ontvangen van GitHub.", { status: 400 });
+  }
+
+  // Vraag het access token op bij GitHub
   const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: {
@@ -12,12 +19,20 @@ export async function onRequestGet(context) {
     },
     body: JSON.stringify({
       client_id: clientId,
+      client_secret: clientSecret,
       code: code
     })
   });
 
   const tokenData = await tokenResponse.json();
   const token = tokenData.access_token;
+
+  if (!token) {
+    return new Response("Fout bij ophalen van token: " + JSON.stringify(tokenData), { 
+      headers: { "content-type": "text/plain" },
+      status: 400 
+    });
+  }
 
   // Stuur het token netjes terug naar het CMS venster
   const html = `
